fe15af7f03ee6fc2a337aca385f764ed15c7024d
Кузнецова Александра <Alexikana8700@gmail.com>
2024-11-06 14:46:25 +0300

    Добавление методов * и /

--- src/Main.java
+++ src/Main.java
@@ -25,14 +25,14 @@ class Complex {
 
     private double x;
     private double y;
-    private double z;
+    private double i;
 
-    public double getZ() {
-        return z;
+    public double getI() {
+        return i;
     }
 
-    public void setZ(double z) {
-        this.z = z;
+    public void setI(double i) {
+        this.i = i;
     }
     String str() {
         return("5+3i");
@@ -46,5 +46,15 @@ class Complex {
         x = x1-x2;
         y = y1-y2;
         System.out.printf("Разность чисел z = (%f, %f)", x, y);
+    }
+    void mul(double x1, double x2, double y1, double y2) {
+        x = x1*x2-y1*y2;
+        y = x1*x2+y1*y2;
+        System.out.printf("Произведение чисел z = (%f, %f)", x, y);
+    }
+    void div(double x1, double x2, double y1, double y2, double i) {
+        x = (x1*x2+y1*y2)/(Math.pow(x2, 2)+Math.pow(y2, 2));
+        y = i*((x2*y1-x1*y2)/(Math.pow(x2, 2)+Math.pow(y2, 2)));
+        System.out.print("Разность чисел z = " + x+y);
     }
 }
