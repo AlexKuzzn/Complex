a95fb71bddee269baee9d3c709d67fc4b3e8c8e4
Кузнецова Александра <Alexikana8700@gmail.com>
2024-11-06 14:35:54 +0300

    Добавление методов + и -

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ class Complex {
 
     private double x;
     private double y;
-    private double z; //Комплексное число
+    private double z;
 
     public double getZ() {
         return z;
@@ -36,5 +36,15 @@ class Complex {
     }
     String str() {
         return("5+3i");
+    }
+    void add(double x1, double x2, double y1, double y2) {
+        x = x1+x2;
+        y = y1+y2;
+        System.out.printf("Сумма чисел z = (%f, %f)", x, y);
+    }
+    void sub(double x1, double x2, double y1, double y2) {
+        x = x1-x2;
+        y = y1-y2;
+        System.out.printf("Разность чисел z = (%f, %f)", x, y);
     }
 }
